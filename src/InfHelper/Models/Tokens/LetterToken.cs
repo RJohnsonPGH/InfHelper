a8dd8d1edc8aaf1e2a33eaadf964ee994002c23a
RJohnsonPGH <RJohnsonPGH@users.noreply.github.com>
2025-11-18 13:42:47 -0500

    Refactory LetterToken so symbols are only computed once instead of every retrieval

--- src/InfHelper/Models/Tokens/LetterToken.cs
+++ src/InfHelper/Models/Tokens/LetterToken.cs
@@ -1,31 +1,34 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 
-namespace InfHelper.Models.Tokens
+namespace InfHelper.Models.Tokens;
+
+/// <summary>
+/// Represents a token that matches any non-control, non-whitespace Unicode letter character.
+/// </summary>
+public class LetterToken : TokenBase
 {
-    public class LetterToken : TokenBase
+    public override char[] Symbols => _symbols.Value;
+
+	// Only compute the symbols once, lazily.
+	private readonly Lazy<char[]> _symbols = new(() =>
     {
-        public override char[] Symbols
+        var symbols = new HashSet<char>();
+        for (int i = char.MinValue; i <= char.MaxValue; i++)
         {
-            get
+            char c = Convert.ToChar(i);
+            if (!char.IsControl(c) && !char.IsWhiteSpace(c))
             {
-                var symbols = new HashSet<char>();
-                for (int i = char.MinValue; i <= char.MaxValue; i++)
-                {
-                    char c = Convert.ToChar(i);
-                    if (!char.IsControl(c) && !char.IsWhiteSpace(c))
-                        symbols.Add(c);
-                }
-                return [.. symbols];
+                symbols.Add(c); 
             }
         }
+        return [.. symbols];
+    });
 
-        public override TokenType Type { get; } = TokenType.Letter;
+    public override TokenType Type { get; } = TokenType.Letter;
 
-        public override bool IsToken(char c)
-        {
-            return !char.IsControl(c) && !char.IsWhiteSpace(c);
-        }
+    public override bool IsToken(char c)
+    {
+        return !char.IsControl(c) && !char.IsWhiteSpace(c);
     }
 }
